1c6fe7ace9f28bcaf9a71a80cb57fdd1d00c07b9
gilbertrendon <jilberlv@gmail.com>
2018-10-27 11:49:23 -0500

    first commit

---
+++ src/CodeBreakerM.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class CodeBreakerM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String result = "";
+		String num = "";
+		CodeBreaker codeBreaker = new CodeBreaker();
+		while(!result.equals("xxxx")){
+			System.out.println("Ingrese un número");
+	        Scanner in = new Scanner (System.in); 
+	        num = in.nextLine();
+			result = codeBreaker.guess(num);
+			System.out.println(result);	
+		}
+		
+		
+	}
+
+}
